3ec34d10f4434f89f60333310d644def4b97293c
Logan Parker <lrparker2@bsu.edu>
2023-08-31 13:00:56 -0400

    Add launch feature to ball

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
+
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("launch"):
+		apply_impulse(Vector2(0,3000))
