89d0b91457861beec68ada1a1aaf4042c671cdfb
Logan Parker <lrparker2@bsu.edu>
2023-08-31 13:29:40 -0400

    Update Launch Feature

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@export var force := 500
 
 func _ready():
 	pass # Replace with function body.
@@ -7,5 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
+	var direction := Vector2(1,-3).normalized()
+	
 	if Input.is_action_just_pressed("launch"):
-		apply_impulse(Vector2(0,3000))
+		apply_impulse(direction * force)
